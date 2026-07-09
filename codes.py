87a903f4f38ed0b07b4857da969e9e72319c53f5
Fayozbek777 <fayozbekbaxromov9@gmail.com>
2026-07-09 13:45:02 +0500

    sql functions

--- codes.py
+++ codes.py
@@ -32,7 +32,7 @@ CREATE TABLE rentals (
     rental_date DATE,
     return_date DATE,
     total_amount INT,
-    status CHAR(24) DEFAULT 'Active', -- Active, Completed, Cancelled
+    status CHAR(24) DEFAULT 'Active',  Active, Completed, Cancelled
     car_id INT,
     client_id INT,
     PRIMARY KEY (id),
@@ -93,3 +93,106 @@ WHERE id = 10;
 
 DELETE FROM clients 
 WHERE id = 1;
+
+
+
+
+
+
+
+ Filter va Aggrogate functions
+
+
+ where  /  between  / like / order by / limit / group by / gt / lt / count / sum / max / min 
+
+
+
+where -> eng assoiy  filter qiluvchi functions 
+
+select * from Track 
+WHERE AlbumId = 2 or/and GenreId  = 2
+
+
+ between > where bn ishlatiladi va oraliqdan malumot ob berad
+
+ select * from Track 
+WHERE Milliseconds  BETWEEN 50000 and 3000000
+WHERE Name BETWEEN "a" and "b"
+WHERE birth_date BETWEEN '1955-15-02' and '2026-01-01'
+
+
+
+ like > yozuvlar ichidan topish imkonini beradi
+
+ select * from Track 
+WHERE Name like 'Fast as a Shark'
+WHERE Name like 'F%'  F blan boshlanadigan Name lar
+WHERE Name like '%A'  va blan tugidigan Name lar
+WHERE Name like '%or%'  agar name ichida or bsa topib berad 
+WHERE lower(Name) like lower('%A') 
+
+
+ order > Sartirovkasini belgilaydi  asc(1..100) desc(100..1)
+ ASC() / DESC() - Ascii jadval bn kelad harf la bn
+
+select * from Track 
+ORDER BY TrackId  DESC
+ 
+
+
+limit > malumotlar olishda limit qoyb beradi va pagintion xosil bolas
+ offset > malum bir miqdorda malumotlarni tashab ketadi
+ Offset har doim limit bn ishlatiladi
+
+select * from Track 
+where (Milliseconds Between 50000 and 100000) or name LIKE '%fast%'
+order by TrackId 
+limit 5
+
+select * from Track 
+where (Milliseconds Between 50000 and 100000) or name LIKE '%fast%'
+order by TrackId 
+limit 5 OFFSET 5
+
+
+
+gt (greather than) 
+lt (lower than)
+
+
+select * from Track
+where Milliseconds  > 100000
+where Milliseconds  < 100000
+
+
+
+
+sum > barcha ustunlarni yigindisinni oladi
+ 
+select 	sum(TrackId )FROM  Track   obsh 6 137 256
+select 	sum(Milliseconds ) FROM Track   obsh  1 378 778 040
+select 	sum(UnitPrice) FROM Track   obsh  3 680,969999997
+
+
+
+ group by > elementlarni ustuniga qarab guruhlarga ajratadi
+
+select sum(UnitPrice) as "Total Price",*from Track 
+GROUP BY GenreId   1 id dagi Genre  1 284 + foyda i td
+
+
+
+Count 
+
+
+select sum(UnitPrice) as "Total Price", COUNT(TrackId) as "Count", *from Track 
+ 1 id dagi albumda 10 ta Track Id bor kan obsh  total price 99$
+GROUP BY GenreId   1 id dagi Genre  1 284 + foyda i td
+
+
+ Max, min > Eng kotta va kichkina qiymatlarni olib beradi
+
+SELECT Max(Milliseconds), * from Track  
+SELECT Min(Milliseconds), * from Track  
+
+
